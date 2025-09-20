6e311747b68f50d72e30425791f72bf7af18e35d
Tanish Shah <36543258+t-shah02@users.noreply.github.com>
2025-09-20 15:09:16 -0600

    Feat/init backend infra (#1)
    * configured s3 backend with lock file for terraform state for deployments
    
    * added aws region variable to root module
    
    * added tf.backend cli argument to tf init step in actions
    
    * reformatted yml files
    
    * Fix indentation in Terraform Init step

---
+++ infrastructure/variables.tf
@@ -0,0 +1,6 @@
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "The AWS region where terraform resources will be deployed for the Dexbooru infrastructure"
+}
