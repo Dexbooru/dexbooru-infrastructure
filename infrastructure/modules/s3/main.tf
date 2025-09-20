7a35948da3f4d1aa50dbb0d3e1effb3d177bfa5d
Tanish Shah <connecttanish@gmail.com>
2025-09-20 15:37:12 -0600

    removed acl rules on s3 buckets

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -4,20 +4,10 @@ resource "aws_s3_bucket" "profile_pictures" {
   region        = var.region
 }
 
-resource "aws_s3_bucket_acl" "profile_picture_bucket_acl" {
-  bucket = aws_s3_bucket.profile_pictures.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket" "post_pictures" {
   bucket        = var.post_picture_bucket_name
   force_destroy = true
   region        = var.region
-}
-
-resource "aws_s3_bucket_acl" "post_picture_bucket_acl" {
-  bucket = aws_s3_bucket.post_pictures.id
-  acl    = "private"
 }
 
 resource "aws_s3_bucket" "collection_pictures" {
@@ -26,7 +16,3 @@ resource "aws_s3_bucket" "collection_pictures" {
   region        = var.region
 }
 
-resource "aws_s3_bucket_acl" "post_collection_bucket_acl" {
-  bucket = aws_s3_bucket.collection_pictures.id
-  acl    = "private"
-}
