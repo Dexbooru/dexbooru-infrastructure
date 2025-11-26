40fdb52d761d5af63848ba88a994ae478395d840
Tanish Shah <connecttanish@gmail.com>
2025-11-26 00:27:31 -0700

    increased queue visibility timeout seconds to higher than the lambda timeout

--- infrastructure/modules/sqs/main.tf
+++ infrastructure/modules/sqs/main.tf
@@ -2,14 +2,16 @@
 locals {
   max_anime_series_classification_message_size_bytes             = 2048
   max_anime_series_classification_message_retention_time_seconds = 86400 # 1 day
+  max_anime_series_classification_visible_time_seconds           = 90    # 1.5 minutes
   anime_series_classification_messages_delay_time_seconds        = 180   # 3 minutes
 }
 
 resource "aws_sqs_queue" "anime_series_classification_queue" {
-  name                      = var.post_images_anime_series_classificarion_queue_name
-  delay_seconds             = local.anime_series_classification_messages_delay_time_seconds
-  message_retention_seconds = local.max_anime_series_classification_message_retention_time_seconds
-  max_message_size          = local.max_anime_series_classification_message_size_bytes
+  name                       = var.post_images_anime_series_classificarion_queue_name
+  delay_seconds              = local.anime_series_classification_messages_delay_time_seconds
+  message_retention_seconds  = local.max_anime_series_classification_message_retention_time_seconds
+  max_message_size           = local.max_anime_series_classification_message_size_bytes
+  visibility_timeout_seconds = local.max_anime_series_classification_visible_time_seconds
 
   tags = {
     Name         = var.post_images_anime_series_classificarion_queue_name
